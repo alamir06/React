a92cbc9696213a532564643b504d3e02fa97777a
Alamirew wagaw <akliluarse@gmail.com>
2025-01-11 09:51:05 -0500

    destructuring array and object complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React ,{useState} from "react";
+import  animals from "./animals";
+
+const [cat,dog]=animals;//array destructuring
+console.log(cat);//similar with animals[0];
+
+const {name,sound}=cat;
+console.log(sound);//similar with animals[0].sound
+
+// give another name for the property
+const {name:dogNmae,sound:dogSound}=dog;
+
+console.log(dogNmae);
+//  give default value to the eproperty
+
+ const {name="miawa",sound="purr"}=cat;
+console.log(sound);
+// nested object destructuring
+const {name,sound,feedingRequirements:{food,water}}=cat;
+console.log(food);
+
+// nested array destructuring
+const {name,sound,numberOflegs:[one,two,three,four,five]}=dog;
+console.log(two);
 
 
 function App() {
 
-  const now=new Date().toLocaleTimeString();
 
-  const [time,setTime]=useState(now);
-  console.log(time)
-
-setInterval(Time,1000);
-function Time()
-{
-  const nowTime=new Date().toLocaleTimeString();
-  setTime(nowTime);
-}
-
-
-  return (
-  <div id="p">
-     <h1>{time}</h1>
-    {/* <button onClick={Time}>+</button> */}
-  </div>
+  return(
+    <div>
+    <h1>hello destructuring</h1>
+    </div>
   );
 }
 
---
+++ src/animals.js
@@ -0,0 +1,15 @@
+const animals=[
+    {name:"cat",
+    sound:"miaw",
+    feedingRequirements:{
+        food:2,
+        water:3
+    }
+    },
+    {name:"dog",
+        sound:"wow",
+        numberOflegs:[1,2,3,4,5]
+        }
+];
+
+export default animals;
