736d558cea194fd91b3f639d9494babe43adab15
Alamirew wagaw <akliluarse@gmail.com>
2025-01-11 21:00:02 -0500

    form handling practice

--- src/App.js
+++ src/App.js
@@ -2,58 +2,69 @@ import React ,{useState} from "react";
 
 
 function App() {
+   
 
-     const [fullName,setFullNmae] = useState({
-        fname:"",
-        lname:""
-     });
-     const [heading,setheading]=useState("");
-
-    function handleInputChange(event)
-    {
-  
-      const {name,value}=event.target;
-
-      setFullNmae(prevalue=>{
-        if(name==="fname")
+  const [contact,setContact] = useState({
+    fname:"",
+    lname:"",
+    email:""
+  });
+   
+function handleInputChange(event){
+     const {name,value}=event.target;
+     setContact((prevalue)=>{
+     if(name==="fname")
+     {
+      return{
+        fname:value,
+        lname:prevalue.lname,
+        email:prevalue.email
+      }
+     }
+     else if(name==="lname")
+      {
+       return{
+         fname:prevalue.fname,
+         lname:value,
+         email:prevalue.email
+       }
+      }
+      else if(name==="email")
         {
-          return {
-            fname:value,
-            lname:prevalue.lname
-          }
+         return{
+           fname:prevalue.fname,
+           lname:prevalue.lname,
+           email:value
+         }
         }
-        else if(name==="lname")
-        {
-          return {
-            fname:prevalue.fname,
-            lname:value
-          }
-        }
-      });
-     
-    }
+        
+       
+    });
+}
 
-    function handleclicked(event)
-    {
-       setheading(fullName.fname +" "+fullName.lname);
 
-       event.preventDefault();
-    }
   return (
   <div className="container">
-      <h1>Hello {heading}</h1>
-      <form  onSubmit={handleclicked}>
+      <h1>Hello {contact.fname} {contact.lname} </h1>
+      <p>{contact.email}</p>
+      <form  >
       <input
        name="fname"
        onChange={handleInputChange} 
        type="text" placeholder="What is your First name?"
-       value={fullName.fname}
+       value={contact.fname}
        /><br /> <br />
        <input
        name="lname"
        onChange={handleInputChange} 
        type="text" placeholder="What is your last name?"
-       value={fullName.lname}
+       value={contact.lname}
+       /><br /> <br />
+        <input
+       name="email"
+       onChange={handleInputChange} 
+       type="email" placeholder="What is your last name?"
+       value={contact.email}
        /><br /> <br />
       <button type="submit" >Submit</button>
       </form>
