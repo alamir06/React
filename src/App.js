061427ada751654215f9e7f51cc2230865085e85
Alamirew wagaw <akliluarse@gmail.com>
2025-01-11 20:41:19 -0500

    form handling complete

--- src/App.js
+++ src/App.js
@@ -3,29 +3,57 @@ import React ,{useState} from "react";
 
 function App() {
 
-  const [name,setName]=useState("");
+     const [fullName,setFullNmae] = useState({
+        fname:"",
+        lname:""
+     });
+     const [heading,setheading]=useState("");
 
-  const [name1,setName1]=useState("");
+    function handleInputChange(event)
+    {
+  
+      const {name,value}=event.target;
 
-   function handleInputChange(event)
-   {
-      setName(event.target.value)
-   }
-   function handleClickedButton(event)
-   {
-  setName1(name);
- 
+      setFullNmae(prevalue=>{
+        if(name==="fname")
+        {
+          return {
+            fname:value,
+            lname:prevalue.lname
+          }
+        }
+        else if(name==="lname")
+        {
+          return {
+            fname:prevalue.fname,
+            lname:value
+          }
+        }
+      });
+     
+    }
 
-   event.preventDefault();
-   }
+    function handleclicked(event)
+    {
+       setheading(fullName.fname +" "+fullName.lname);
+
+       event.preventDefault();
+    }
   return (
   <div className="container">
-      <h1>Hello {name1}</h1>
-      <form onSubmit={handleClickedButton}>
+      <h1>Hello {heading}</h1>
+      <form  onSubmit={handleclicked}>
       <input
+       name="fname"
        onChange={handleInputChange} 
-       type="text" placeholder="What is your name?"
-       value={name}
+       type="text" placeholder="What is your First name?"
+       value={fullName.fname}
+       /><br /> <br />
+       <input
+       name="lname"
+       onChange={handleInputChange} 
+       type="text" placeholder="What is your last name?"
+       value={fullName.lname}
        /><br /> <br />
       <button type="submit" >Submit</button>
       </form>
